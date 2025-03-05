263e9fb2297cd3bbf145b141c18aa0dbfb00690a
Lucas <68867449+Luke02su@users.noreply.github.com>
2025-03-05 09:21:11 -0300

    PageWithNoLikes-FacebookSQLInterviewQuestion.sql

--- PageWithNoLikes-FacebookSQLInterviewQuestion.sql
+++ PageWithNoLikes-FacebookSQLInterviewQuestion.sql
@@ -10,4 +10,14 @@ SELECT p.page_id
 FROM pages p
 WHERE p.page_id NOT IN (
   SELECT pl.page_id
-  FROM page_likes pl);
+  FROM page_likes pl
+);
+
+-- Example 3:
+SELECT p.page_id
+FROM pages p
+WHERE NOT EXISTS (
+  SELECT 1 -- Value true
+  FROM page_likes pl
+  WHERE p.page_id = pl.page_id
+);
