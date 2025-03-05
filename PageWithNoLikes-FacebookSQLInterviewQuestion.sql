bee59a8c9fbc1ef287c1ba9f00b6bcadb7e5c153
Lucas <68867449+Luke02su@users.noreply.github.com>
2025-03-05 09:14:22 -0300

    PageWithNoLikes-FacebookSQLInterviewQuestion.sql

--- PageWithNoLikes-FacebookSQLInterviewQuestion.sql
+++ PageWithNoLikes-FacebookSQLInterviewQuestion.sql
@@ -1,5 +1,13 @@
+-- Example 1:
 SELECT p.page_id
 FROM pages p
 LEFT JOIN page_likes pl
 ON p.page_id = pl.page_id
 WHERE pl.page_id IS NULL;
+
+-- Example 2:
+SELECT p.page_id
+FROM pages p
+WHERE p.page_id NOT IN (
+  SELECT pl.page_id
+  FROM page_likes pl);
