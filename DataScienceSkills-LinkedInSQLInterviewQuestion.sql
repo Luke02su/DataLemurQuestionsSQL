9f3da602fc1e3efc044c8a69f73eb5b7766ca2a1
Lucas <68867449+Luke02su@users.noreply.github.com>
2025-03-05 09:02:47 -0300

    Update and rename LinkedInSQLInterviewQuestion.sql to DataScienceSkills-LinkedInSQLInterviewQuestion.sql

--- DataScienceSkills-LinkedInSQLInterviewQuestion.sql
+++ DataScienceSkills-LinkedInSQLInterviewQuestion.sql
@@ -1,6 +1,6 @@
 SELECT candidate_id
 FROM candidates
 WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
-GROUP BY candidate_id -- GROUP BY ve antes de HAVING
+GROUP BY candidate_id -- GROUP BY vem antes de HAVING
 HAVING COUNT(skill) = 3 -- HAVING é usado após o agrupamento
 ORDER BY candidate_id ASC;
